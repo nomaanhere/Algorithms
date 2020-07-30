a01cdc3dd2c9d3e49061d4d8661e09197a4ea1e8
Nomaan Parmar <mohamadnomaan.p@somaiya.edu>
2020-07-30 19:52:56 +0530

    Update lruCache.java

--- LRU Cache/lruCache.java
+++ LRU Cache/lruCache.java
@@ -1,1 +1,14 @@
+//LRU cache can be implemented using a doublyLinkedList along with a hashmap
+//LRU cache implementation uses two APIs viz. get() & put() methods
 
+
+public cacheEntry(int key, String value){
+  int key;
+  String value;
+  cacheEntry(int key){
+    this.key = key;
+    this.value = value;
+  }
+}
+HashMap<Integer,cacheEntry> entry = new HashMap();
+public void 
