1986782655ce79b67af641b7317a9cf6a1184c74
Nomaan Parmar <mohamadnomaan.p@somaiya.edu>
2020-07-30 20:52:46 +0530

    Update insertionSort.java

--- Sorting/Insertion Sort/insertionSort.java
+++ Sorting/Insertion Sort/insertionSort.java
@@ -1,1 +1,15 @@
-
+public static void main(String args[]){
+int arr[] = {5,4,3,2,1};
+for(int i=1;i<n;i++){
+ int temp = arr[i];
+  j = i-1;
+  while(j>=0 && arr[j]>temp){
+    arr[j+1]=arr[j];
+    j--;
+  }
+  arr[j+1]=temp;
+}
+  for(int i : arr){
+    System.out.print(i+" ");
+  }
+}
