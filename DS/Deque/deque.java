5bbdbc93213f8c2919dae0b49b3232386473f02d
Nomaan Parmar <mohamadnomaan.p@somaiya.edu>
2020-07-30 17:36:47 +0530

    Update deque.java

--- DS/Deque/deque.java
+++ DS/Deque/deque.java
@@ -1,1 +1,10 @@
+-> Deque which is also known as Doubly Ended Queue.
+Note:
+The major difference between the normal queue and the deque is that the node/data can be inserted/deleted from both the FRONT and the REAR of the queue.
+  
+Doubly Ended Queue can be implemented using a Doubly LinkedList.
 
+Applications:
+-> DoublyEnded queue can be used to implement LRU cache where the insertion/deletion of the node from the front/rear pointer is performed in O(1) Time Complexity.
+  
+  
